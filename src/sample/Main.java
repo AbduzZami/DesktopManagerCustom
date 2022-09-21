1eea0959e284ec9a7c859b7717175f974a9a10eb
Abduz Zami <69592754+AbduzZami@users.noreply.github.com>
2022-09-21 19:22:39 +0600

    safe

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -15,6 +16,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Desktop Manager");
         primaryStage.setScene(new Scene(root, 356, 296));
+        primaryStage.getIcons().add(new Image("file:assets/logo.png"));
         primaryStage.show();
     }
 
